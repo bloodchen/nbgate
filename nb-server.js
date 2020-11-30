84b354f878322a96c59018ff32675b504735c85a
Glitch (nbgate) <none>
2020-11-30 03:11:34 +0000

    update readme

--- nb-server.js
+++ nb-server.js
@@ -1,11 +1,6 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-//const fileUpload = require("express-fileupload");
+
 require('dotenv').config();
 
 const nbMgr = require("./nbweb_mgr.js");
@@ -13,31 +8,8 @@ const bitfs = require("./bitfs.js");
 const ipfs = require("./ipfs.js");
 
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-/*app.use(
-  fileUpload({
-    createParentPath: true,
-    debug: true,
-    limits: {
-      fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
-    }
-  })
-); */
-/*app.use(function(req, res, next) {
-  if (req.originalUrl.slice(-5) === '.test') {
-     var fullUrl = req.originalUrl;
-    console.log("xxxx:"+req.originalUrl);
-    //res.end(fullUrl);
-    req.url = req.originalUrl+"/";
-    res.redirect(302, req.url);
-    
-  }
-  next();
-});
-*/
 app.get("/bitfs/*", async (req, res) => {
   //response.sendFile(__dirname + "/views/index.html");
   console.log(req.path);
@@ -55,23 +27,6 @@ app.get("/ipfs/*", async (req, res) => {
   } 
 });
 
-// https://expressjs.com/en/starter/basic-routing.html
-function get_tld_path(tld,url){
-  let arr = url.split('/');
-  let tld_len = tld.length;
-  console.log(arr);
-  let tld_pos = 0;
-  for(let i=0;i<arr.length;i++){
-    if(arr[i].slice(-tld_len)===tld){
-      tld_pos = i;
-    }
-  }
-  let path = arr[tld_pos];
-  for(let j=tld_pos+1;j<arr.length;j++){
-    path+="/"+arr[j];
-  }
-  return path;
-} 
 app.get("/_mark",async (req,res)=>{
   res.sendFile(__dirname+"/template/text.html");  
 })
@@ -87,9 +42,6 @@ app.get("/*", async (req, res) => {
   console.log("url=",url);
   if(url.indexOf('.test')!=-1||url.indexOf('.b')!=-1)
   {
-    
-    //const tld_path = get_tld_path(".test",req.path);
-    //console.log("tld_path="+tld_path);
     const tld_path = url;
     nbMgr.handleURL(res,tld_path);
   }else{
